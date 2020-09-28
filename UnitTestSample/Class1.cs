ebf5fe0ac811e2e892e69090d6b0eb63adf108d8
VB <vinayak.bagalkoti@accionlabs.com>
2020-09-28 13:04:07 +0530

    Unit Test - CICD

---
+++ UnitTestSample/Class1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSample
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void NunitTestMethod1()
+        {
+            Assert.AreEqual(20, 20);
+        }
+
+        [Test]
+        public void NunitTestMethod2()
+        {
+            Assert.AreEqual(50, 50);
+        }
+    }
+}
