60159398628929d55aa7e4ecc1bf13abe984a1f1
VB <vinayak.bagalkoti@accionlabs.com>
2020-10-05 13:14:12 +0530

    Added Silenium Test Cases - CICD - Commit

---
+++ SeleniumFunctionalTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumFunctionalTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SeleniumFunctionalTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a7c8b96-145c-4875-9fa6-2970f5b94270")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumFunctionalTests/UnitTest1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFunctionalTests
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        //test
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("http://3.128.91.93/WebApplication1_Deploy");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+            String ActualTitle = driver.FindElement(By.XPath("/html/head/title")).Text;
+            String ExpectedTitle = "Home Page - My ASP.NET Application";
+            Assert.AreEqual(ExpectedTitle, ActualTitle);
+            driver.Close();
+        }
+
+        // Testing whether the Home link exits or not
+
+        //[Test]
+        //public void Test2()
+        //{
+        //    driver.Navigate().GoToUrl("http://3.128.91.93/WebApplication1_Deploy");
+        //    driver.Manage().Window.Maximize();
+        //    Thread.Sleep(2000);
+        //    //driver.FindElement(By.XPath("//*[contains(text(),'MVC_Core_NUnit')]")).Click();
+        //    //String ActualTitle = driver.FindElement(By.XPath("//*[contains(text(),'Home')]")).Text;
+        //    String ActualTitle = driver.FindElement(By.XPath("/html/body/header/nav/div/div/ul/li[1]/a")).Text;
+        //    String ExpectedTitle = "Home";
+        //    Assert.AreEqual(ExpectedTitle, ActualTitle);
+        //    driver.Close();
+        //}
+
+        //[Test]
+        //public void Test3()
+        //{
+        //    driver.Navigate().GoToUrl("http://3.128.91.93/WebApplication1_Deploy");
+        //    driver.Manage().Window.Maximize();
+        //    Thread.Sleep(2000);
+        //    driver.FindElement(By.XPath("//*[contains(text(),'Privacy')]")).Click();
+        //    Thread.Sleep(2000);
+        //    //driver.FindElement(By.XPath("//*[contains(text(),'Privacy')]")).Click();
+
+        //    //string text = driver.FindElement(By.XPath("/html/body/div/main/h1")).Text;
+
+        //    // Console.WriteLine(text);
+
+        //    // Console.WriteLine(driver.FindElement(By.XPath("/html/body/div/main/h1")).Text);
+
+        //    String ActualTitle = driver.FindElement(By.XPath("/html/body/div/main/h1")).Text;
+        //    String ExpectedTitle = "Privacy Policy";
+        //    Assert.AreEqual(ExpectedTitle, ActualTitle);
+        //    driver.Close();
+        //    //Assert.Pass();
+
+        //}
+
+        //[Test]
+        //public void Test4()
+        //{
+        //    driver.Navigate().GoToUrl("https://localhost:44371");
+        //    driver.Manage().Window.Maximize();
+        //    Thread.Sleep(2000);
+        //    String ActualTitle = driver.FindElement(By.XPath("/html/body/div/main/div/h1")).Text;
+        //    String ExpectedTitle = "Welcome Anoop";
+        //    Assert.AreEqual(ExpectedTitle, ActualTitle);
+        //    driver.Close();
+        //}
+
+
+
+        //[Test]
+        //public void Test5()
+        //{
+        //    driver.Navigate().GoToUrl("https://localhost:44371");
+        //    driver.Manage().Window.Maximize();
+        //    Thread.Sleep(2000);
+        //    String ActualTitle = driver.FindElement(By.XPath("/html/body/div/main/div/table/tbody/tr[1]/td[1]/span/b")).Text;
+        //    String ExpectedTitle = "Employe ID:";
+        //    Assert.AreEqual(ExpectedTitle, ActualTitle);
+        //    driver.Close();
+        //}
+
+
+
+        //[Test]
+        //public void Test6()
+        //{
+
+        //    driver.Navigate().GoToUrl("https://localhost:44371");
+        //    driver.Manage().Window.Maximize();
+        //    Thread.Sleep(2000);
+        //    String ActualTitle = driver.FindElement(By.XPath("/html/body/div/main/div/table/tbody/tr[1]/td[2]")).Text;
+        //    String ExpectedTitle = "3236";
+        //    Assert.AreEqual(ExpectedTitle, ActualTitle);
+        //    driver.Close();
+        //}        
+
+
+        /*[Test]
+
+        public void Test2()
+
+        {
+
+            driver.Navigate().GoToUrl("https://twitter.com/login");
+
+            driver.Manage().Window.Maximize();
+
+            Thread.Sleep(2000);
+
+            driver.Close();
+
+            //Assert.Pass();
+
+        }
+
+        [Test]
+
+        public void Test3()
+
+        {
+
+            driver.Navigate().GoToUrl("https://google.com/");
+
+            driver.Manage().Window.Maximize();
+
+            Thread.Sleep(2000);
+
+            driver.Close();
+
+            //Assert.Pass();
+
+        }
+
+        */
+
+    }
+}
